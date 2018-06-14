302c331a779f02b593b678df0f5bbb3e93d0ba21
Sergio Ferrete <sergioferretebenitez@gmail.com>
2018-06-15 00:55:56 +0200

    Added openform models

--- openform/openform/settings.py
+++ openform/openform/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for openform project.
 
@@ -31,7 +32,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    # 'models',
+    'openform',
+    # 'models.app.ModelsConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
