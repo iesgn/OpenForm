df46e0361890c55fda70a142715cfa3bfeab590a
Sergio Ferrete <sergioferretebenitez@gmail.com>
2018-06-19 08:23:46 +0200

    fixed old version issue

--- openform/generator/urls.py
+++ openform/generator/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_plan', views.new_plan, name="new_plan"),
+    # path('new_plan', views.new_plan, name="new_plan"),
     path('new_instance_plan', views.new_instance_plan, name="new_instance_plan"),
     path('new_network_plan', views.new_network_plan, name="new_network_plan"),
     path('_genplan', views._genplan, name="_genplan"),
